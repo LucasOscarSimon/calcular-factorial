1b1a23d204102d3435636c5883d5c89e477195be
LucasSimon <LucasSimon@DESKTOP-3QPOJ8Q>
2017-08-22 22:55:10 -0300

    Agregar archivos de proyecto.

---
+++ Factoriales/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CalculoFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Calculo del Factorial de un número entero positivo\n");
+            do
+            {
+                Console.Write("Por favor Digite un número entero positivo: ");
+                num = int.Parse(Console.ReadLine());
+                verificar(num);
+            } while (num < 0);
+            factorial(num);
+            Console.ReadKey();
+        }
+        static void verificar(int n)
+        {
+            if (n < 0)
+            {
+                Console.Write("\nError: No existe el factorial de un número negativo...\n");
+            }
+        }
+        static void factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+        }
+    }
+}
